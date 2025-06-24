83dcdd9913465e765c67a1a775142c00c0e71b43
Mohit <bluesky5april2021@gmail.com>
2025-06-25 02:38:17 +0530

    added share button

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -70,7 +70,19 @@ export class ProfileComponent implements OnInit {
     }
   }
   
+  shareWeb(){
+    const text = "Check out my portfolio!\nThis is the personal portfolio of se00n00 — showcasing my work, projects, and skills.\nhttps://se00n00.github.io/My_Portfolio/";
+
+    navigator.clipboard.writeText(text).then(() => {
+      alert("Text copied to clipboard!");
+    }).catch(err => {
+      alert("Failed to copy text: " + err);
+    });
+  }
   
+  toggleDarkMode(){
+    document.documentElement.classList.toggle('dark');
+  }
 
   loadProfileData(): void {
     this.http.get<ProfileData>('data/profile.json')
