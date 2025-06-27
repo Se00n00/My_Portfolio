492a328610fe7b9edeeab1849dc5ba31c5ad7b37
Mohit <bluesky5april2021@gmail.com>
2025-06-27 11:10:50 +0530

    added filter menu for project section

--- src/app/sub_pages/projects/projects.component.ts
+++ src/app/sub_pages/projects/projects.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 interface Project {
   title: string;
@@ -20,13 +21,29 @@ interface Project {
   styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent implements OnInit {
+  project_types = ["MACHINE LEARNING","WEB DEVELOPMENT","CORE"]
   projects: Project[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.getData();
+    });
+  }
+  getData(){
     this.http.get<Project[]>('data/projects.json').subscribe((data) => {
       this.projects = data;
     });
   }
+  filterData(input: string) {
+    if (input === "ALL") {
+      this.getData();
+    } else {
+      this.http.get<Project[]>('data/projects.json').subscribe((data) => {
+        this.projects = data.filter(project => project.label === input);
+      });
+    }
+  }
+
 }
